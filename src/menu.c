0db8f7999270e87848d5e79712c842ba13e0f961
JoeOwino <Joeoowino@gmail.com>
2025-05-08 01:08:19 +0300

    refactor: changed the ifinite loop for checking invalid selection in initMenu function

--- src/menu.c
+++ src/menu.c
@@ -52,44 +52,52 @@ void mainMenu(struct User u)
     }
 };
 
+
+
 void initMenu(struct User *u)
 {
     int r = 0;
     int option;
-    system("clear");
-    printf("\n\n\t\t======= ATM =======\n");
-    printf("\n\t\t-->> Feel free to login / register :\n");
-    printf("\n\t\t[1]- login\n");
-    printf("\n\t\t[2]- register\n");
-    printf("\n\t\t[3]- exit\n");
-    while (!r)
+    
+    do 
     {
+        system("clear");
+    
+        printf("\n\n\t\t======= ATM =======\n");
+        printf("\n\t\t-->> Feel free to login / register :\n");
+        printf("\n\t\t[1]- login\n");
+        printf("\n\t\t[2]- register\n");
+        printf("\n\t\t[3]- exit\n");    
+
         scanf("%d", &option);
-        switch (option)
+
+    } while (option < 1 || option > 3);
+       
+    switch (option)
+    {
+    case 1:
+        loginMenu(u->name, u->password);
+        if (strcmp(u->password, getPassword(*u)) == 0)
         {
-        case 1:
-            loginMenu(u->name, u->password);
-            if (strcmp(u->password, getPassword(*u)) == 0)
-            {
-                printf("\n\nPassword Match!");
-            }
-            else
-            {
-                printf("\nWrong password!! or User Name\n");
-                exit(1);
-            }
-            r = 1;
-            break;
-        case 2:
-            // student TODO : add your **Registration** function
-            // here
-            r = 1;
-            break;
-        case 3:
-            exit(1);
-            break;
-        default:
-            printf("Insert a valid operation!\n");
+            printf("\n\nPassword Match!");
         }
+        else
+        {
+            printf("\nWrong password!! or User Name\n");
+            exit(1);
+        }
+        r = 1;
+        break;
+    case 2:
+        // student TODO : add your **Registration** function
+        // here
+        r = 1;
+        break;
+    case 3:
+        exit(1);
+        break;
+    default:
+        printf("Insert a valid operation!\n");
     }
+    
 };
