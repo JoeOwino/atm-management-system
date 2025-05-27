019470691fb6ffb67a9abdcea499c453b69069e7
JoeOwino <joeoowino@gmail.com>
2025-05-27 10:52:10 +0300

    feat: remove checkAllAccounts function to streamline account management

--- src/system.c
+++ src/system.c
@@ -249,32 +249,3 @@ void createNewAcc(struct User u)
     fclose(pf);
     success(u);
 }
-
-void checkAllAccounts(struct User u)
-{
-    char userName[100];
-    struct Record r;
-
-    FILE *pf = fopen(RECORDS, "r");
-
-    system("clear");
-    printf("\t\t====== All accounts from user, %s =====\n\n", u.name);
-    while (getAccountFromFile(pf, userName, &r))
-    {
-        if (strcmp(userName, u.name) == 0)
-        {
-            printf("_____________________\n");
-            printf("\nAccount number:%d\nDeposit Date:%d/%d/%d \ncountry:%s \nPhone number:%s \nAmount deposited: $%.2f \nType Of Account:%s\n",
-                   r.accountNbr,
-                   r.deposit.day,
-                   r.deposit.month,
-                   r.deposit.year,
-                   r.country,
-                   r.phone,
-                   r.amount,
-                   r.accountType);
-        }
-    }
-    fclose(pf);
-    success(u);
-}
