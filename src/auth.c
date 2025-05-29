9c7b033a130919a35b68b2e8d93937da7a53ff0b
JoeOwino <joeoowino@gmail.com>
2025-05-29 09:34:17 +0300

    feat: improve loginMenu function with enhanced input handling and error messages

--- src/auth.c
+++ src/auth.c
@@ -3,18 +3,16 @@
 char *USERS = "./data/users.txt";
 
 void loginMenu(char name[50], char pass[50])
-{    
+{
     struct termios oflags, nflags;
     struct User u;
-
-
 
     while (1)
     {
         system("clear");
         printf("\n\n\t\t=======================  ATM  ==========================\n");
         printf("\t\t                    Login to continue \n");
-        printf("\t\t  Yuo can exit the system at any point by prssing ctr+c\n");
+        printf("\t\t  You can exit the system at any point by pressing ctrl+c\n");
         printf("\t\t==========================================================\n");
 
         printf("\n\t\tEnter Username: ");
@@ -22,53 +20,75 @@ void loginMenu(char name[50], char pass[50])
         if (!validateStringInput(name, 50)) {
             printf("\t\tError reading input\n");
             printf("\t\tPress enter to continue....");
-            fflush(stdout); 
             getchar(); 
-            continue; 
+            continue;
         }
 
         if (isContainsSpaces(name)) {
             printf("\t\tInvalid name. Please enter a valid name without spaces.\n");
             printf("\t\tPress enter to continue....");
-            fflush(stdout); 
             getchar(); 
-            continue; 
+            continue;
         }
 
         strcpy(u.name, name);
 
         if (isUniqueName(name, &u)) {
-            printf("\t\tUser whith name %s not found.\n", name);
+            printf("\t\tUser with name %s not found.\n", name);
             printf("\t\tPress enter to continue....");
-            fflush(stdout); 
             getchar(); 
-            continue; 
-        }        
+            continue;
+        }
 
         break;
     }
-    
-    // disabling echo
-    tcgetattr(fileno(stdin), &oflags);
+
+    // disable echo
+    if (tcgetattr(fileno(stdin), &oflags) != 0) {
+        perror("tcgetattr");
+        exit(1);
+    }
     nflags = oflags;
     nflags.c_lflag &= ~ECHO;
     nflags.c_lflag |= ECHONL;
 
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
         perror("tcsetattr");
-        return exit(1);
+        exit(1);
     }
-    printf("\n\n\n\n\n\t\t\t\tEnter the password to login: ");
-    scanf("%s", pass);
 
-
-    // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    while (1)
     {
+        system("clear");
+        printf("\n\n\t\t=======================  ATM  ==========================\n");
+        printf("\t\t                   Login to continue \n");
+        printf("\t\t  You can exit the system at any point by pressing ctrl+c\n");
+        printf("\t\t==========================================================\n");
+
+        printf("\n\t\tEnter Username: %s", name);
+        printf("\n\n\t\tEnter the password to login: ");
+
+        if (!fgets(pass, 50, stdin)) {
+            printf("\n\t\tError reading password\n");
+            continue;
+        }
+
+        // Remove trailing newline from fgets
+        pass[strcspn(pass, "\n")] = 0;
+
+        if (strcmp(pass, getPassword(&u)) != 0) {
+            printf("\n\t\tWrong password!\n");
+            printf("\t\tPress enter to continue....");
+            getchar();
+            continue;
+        }
+
+        break;
+    }
+
+    // restore terminal settings
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
         perror("tcsetattr");
-        return exit(1);
+        exit(1);
     }
 }
-
-
