9c2068f2a8efec08535ee90d17fbc84ef5456b73
JoeOwino <joeoowino@gmail.com>
2025-06-02 10:13:45 +0300

    refactor: fix format specifier for account type in fscanf

--- src/docs.c
+++ src/docs.c
@@ -167,7 +167,7 @@ bool getAccount(int acc, struct Record *r, struct User *u, int userID)
         return false;
     }
     
-    while (fscanf(fp, "%d %d %s %d %d/%d/%d %s %s %lf %s\n",
+    while (fscanf(fp, "%d %d %s %d %d/%d/%d %s %s %.2lf %s\n",
            &r->id, &u->id, u->name, &r->accountNbr,
            &r->deposit.month, &r->deposit.day, &r->deposit.year,
            r->country, r->phone, &r->amount, r->accountType) == 11) {
